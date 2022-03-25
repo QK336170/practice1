96e355519a0bafcbf69aaffe36edb55b18bb5a38
QK336170 <102235828+QK336170@users.noreply.github.com>
2022-03-26 01:06:30 +0200

    Add files via upload

---
+++ practice1-login/app.py
@@ -0,0 +1,120 @@
+from flask import Flask, flash, render_template, request, redirect, url_for, session, send_from_directory
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+  
+app = Flask(__name__)
+  
+app.secret_key = 'your secret key'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'lili'
+app.config['MYSQL_DB'] = 'logindb'
+  
+mysql = MySQL(app)
+  
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+  
+value = ''
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form  :
+        username = request.form['username']
+        password = request.form['password']
+        
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s)', (username, password, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+            return render_template('login.html')
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
+
+UPLOAD_FOLDER = './static/uploads'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print('upload_image filename: ' + os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #prints('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename))
+
+
+
+if __name__=='__main__':
+    app.run(debug=True) 
+
